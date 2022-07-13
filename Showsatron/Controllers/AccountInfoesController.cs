24bf2d886c14aee102de5fc178547aec34896ce8
Twisha Chachra <twishachachra@gmail.com>
2022-07-13 15:57:23 -0400

    Disable account confirmation by email. Created account with the provided credentials. Made authorization changes.

--- Showsatron/Controllers/AccountInfoesController.cs
+++ Showsatron/Controllers/AccountInfoesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using Showsatron.Models;
 
 namespace Showsatron.Controllers
 {
+    [Authorize]
     public class AccountInfoesController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -26,6 +28,7 @@ namespace Showsatron.Controllers
             return View(await applicationDbContext.ToListAsync());
         }
 
+        [AllowAnonymous]
         // GET: AccountInfoes/Details/5
         public async Task<IActionResult> Details(int? id)
         {
--- Showsatron/Controllers/GenresController.cs
+++ Showsatron/Controllers/GenresController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using Showsatron.Models;
 
 namespace Showsatron.Controllers
 {
+    [Authorize]
     public class GenresController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -25,6 +27,7 @@ namespace Showsatron.Controllers
             return View(await _context.Genres.ToListAsync());
         }
 
+        [AllowAnonymous]
         // GET: Genres/Details/5
         public async Task<IActionResult> Details(int? id)
         {
